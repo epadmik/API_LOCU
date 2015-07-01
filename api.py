e1f497667e70409c5d53c6b53488f7556d7acd8b
epadmik <smcrawford85@gmail.com>
2015-07-01 01:22:05 -0700

    add csv functionality

--- api.py
+++ api.py
@@ -1,20 +1,24 @@
 import urllib2
 import json
 import pdb
+import csv
 
 
+#LOCU API key
 locu_api = 'f087ffd8e0a495f6ac6f5df6885fca48d3c1e22b'
-url = "https://api.locu.com/v1_0/venue/search/?api_key=f087ffd8e0a495f6ac6f5df6885fca48d3c1e22b"
+#url = "https://api.locu.com/v1_0/venue/search/?api_key=f087ffd8e0a495f6ac6f5df6885fca48d3c1e22b"
+
+master_arry = []
 
 
 
 
-def locationSearch(location):
+def locationSearch(location, category):
 	api_key = locu_api
 	url = 'https://api.locu.com/v1_0/venue/search/?api_key=' + api_key 
 	locality = location.replace(' ','%20')
 
-	final_url = url +  '&locality=' + locality + "&category=restaurant"
+	final_url = url +  '&locality=' + locality + "&category=" + category
 	
 	help = urllib2.urlopen(final_url)
 
@@ -22,13 +26,23 @@ def locationSearch(location):
 
 	for items in json_data['objects']:
 		print(items['name'].encode('ascii', 'ignore'),items['phone'].encode('ascii', 'ignore'),items['street_address'].encode('ascii', 'ignore'))
+		master_arry.append([items['name'].encode('ascii', 'ignore'),items['phone'].encode('ascii', 'ignore'),items['street_address'].encode('ascii', 'ignore')])
+
+		#csv.write(items['name'].encode('ascii', 'ignore'),items['phone'].encode('ascii', 'ignore'),items['street_address'].encode('ascii', 'ignore'))
+
+	
+	with open("data.csv", "wb") as f:
+		writer = csv.writer(f)
+		writer.writerows(master_arry)
 		
 
 print("Welcome to my Restaurant API")
 x = raw_input("Please enter a city: ")
+y = raw_input("Please enter a category ex: SPA, Restaurant, Gym: ")
+
 
 print("Here are the restaurant in " + str(x))
-locationSearch(x)
+locationSearch(x,y)
 
 #print("Restaurants New York")
 #locationSearch("New York")
