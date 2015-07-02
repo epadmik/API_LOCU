bf76837f77eb0ef8f37571bf83766fb69a9764d5
Marc C. <smcrawford85@gmail.com>
2015-07-01 19:15:35 -0700

    Update api.py
    removed key

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ import csv
 
 
 #LOCU API key
-locu_api = 
-#url = "https://api.locu.com/v1_0/venue/search/?api_key=f087ffd8e0a495f6ac6f5df6885fca48d3c1e22b"
+locu_api = "add key to work"
+
 
 master_arry = []
 
